08cf31b84ee3f59f0dd05b64fb95ff8243d6d204
Pr0duktiv <patrice@5becker.de>
2017-09-09 23:25:47 +0200

    Added code for parsing all available rooms:

--- Developer/zurich_zimmer/monitor_studentvillage.py
+++ Developer/zurich_zimmer/monitor_studentvillage.py
@@ -3,9 +3,27 @@ import requests
 
 class Monitoring:
     url = 'http://studentvillage.ch/unterkunfte/'
+    treepath = '//table[@class="wohnen_table type_is_all"]/tbody/tr'
 
-    def requestPage(url):
-        page = requests.get(url)
+    def requestPage(self):
+        page = requests.get(self.url)
         tree = html.fromstring(page.content)
+        rows = tree.xpath(self.treepath)
+        result = list()
 
-        print(tree)
+        for row in rows:
+            result.append([x.text for x in row.getchildren()])
+        return result
+
+    def lookForFreeRooms(self, tablecontent):
+        for element in tablecontent:
+            print(element)
+
+    def sendApplication(self, destination):
+        print("Sending Application")
+
+
+if __name__ == "__main__":
+    monObj = Monitoring()
+    result = monObj.requestPage()
+    monObj.lookForFreeRooms(result)
