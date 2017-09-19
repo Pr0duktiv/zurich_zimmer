6afc1178d91b393ae290050c0cc29d2f1830ada4
Pr0duktiv <patrice@5becker.de>
2017-09-19 20:41:13 +0200

    Working email sending

--- Developer/zurich_zimmer/monitor_studentvillage.py
+++ Developer/zurich_zimmer/monitor_studentvillage.py
@@ -1,3 +1,4 @@
+import smtplib
 from lxml import html
 import requests
 
@@ -5,6 +6,7 @@ class Monitoring:
     url = 'http://studentvillage.ch/unterkunfte/'
     treepath = '//table[@class="wohnen_table type_is_all"]/tbody/tr'
     limitprice = 1000
+    targetemail = "patrice@5becker.de"
 
     def requestPage(self):
         page = requests.get(self.url)
@@ -26,18 +28,30 @@ class Monitoring:
         return result
 
     def lookForFreeRooms(self, tablecontent):
+        freerooms = ''
         for element in tablecontent:
             totalprice = int(element[4][4:-2])
             occupation = element[7]
             #status = "Reserviert"
             status = "Verfügbar"
             if occupation == status and totalprice <= self.limitprice:
-                print(element[len(element)-1])
+                freerooms += (element[len(element)-1]) + '\n'
+        return freerooms
 
-    def sendMail(self):
-        print("Sending Application")
+    def sendMail(self, message):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.connect("smtp.gmail.com",587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login("zurichzimmergithub@gmail.com", "amyr8apm8cy3")
+        msg = "Hello! There are the following free rooms at StudentVillage: " + message
+        server.sendmail("zurichzimmergithub@gmail.com", self.targetemail, msg)
 
 if __name__ == "__main__":
     monObj = Monitoring()
+
     result = monObj.requestPage()
-    monObj.lookForFreeRooms(result)
+    freerooms = monObj.lookForFreeRooms(result)
+    if len(freerooms) > 0:
+        monObj.sendMail(freerooms)
